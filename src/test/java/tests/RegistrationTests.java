73f2fb860c46b139f2a9a5d7ba6b1390209a3b8e
sergeyplechko <sergeyplechko@gmail.com>
2025-04-25 16:09:27 +0300

    Added classes: RegistrationPage.java, RegistrationTests.java, RegistrationWithPageObjectsTests.java

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -13,37 +13,38 @@ public class RegistrationWithPageObjectsTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
-        @Test
-        void successfulRegistrationTest() {
+    @Test
+    void successfulRegistrationTest() {
 
-            registrationPage.openPage();
+        registrationPage.openPage();
 
-            registrationPage.setFirstName("Alex");
-            registrationPage.setLastName("Egorov");
+        registrationPage.setFirstName("Alex");
+        registrationPage.setLastName("Egorov");
 
-            $("#firstName").setValue("Alex");
-            $("#lastName").setValue("Egorov");
-            $("#userEmail").setValue("alex@egorov.com");
-            $("#genterWrapper").$(byText("Other")).click();
-            $("#userNumber").setValue("1234567890");
-            $("#dateOfBirthInput").click();
-            $(".react-datepicker__month-select").selectOption("July");
-            $(".react-datepicker__year-select").selectOption("2008");
-            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
-            $("#subjectsInput").setValue("Math").pressEnter();
-            $("#hobbiesWrapper").$(byText("Sports")).click();
-            $("#uploadPicture").uploadFromClasspath("img/1.png");
-            $("#currentAddress").setValue("Some address 1");
-            $("#state").click();
-            $("#stateCity-wrapper").$(byText("NCR")).click();
-            $("#city").click();
-            $("#stateCity-wrapper").$(byText("Delhi")).click();
-            $("#submit").click();
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
 
-            $(".modal-dialog").should(appear);
-            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-            $(".table-responsive").shouldHave(text("Alex"), text("Egorov"),
-                    text("alex@egorov.com"), text("1234567890"));
-        }
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex"), text("Egorov"),
+                text("alex@egorov.com"), text("1234567890"));
     }
 }
+}
+
