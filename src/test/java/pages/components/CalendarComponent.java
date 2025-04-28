f01e71fd995edc823b308bd882c1334b0a45f87c
sergeyplechko <sergeyplechko@gmail.com>
2025-04-28 18:01:38 +0300

    Updated

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,14 +1,15 @@
 package pages.components;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
 
     public void setDate(String day, String month, String year) {
-        $(".react-datepicker__month-select").selectOption("May");
-        $(".react-datepicker__year-select").selectOption("2001");
-        $$("div.react-datepicker__day").findBy(text("1")).click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $$("div.react-datepicker__day").findBy(text(day)).click();
     }
 }
