aba375abd90ae2e23f243834a924e492c10ba240
sergeyplechko <sergeyplechko@gmail.com>
2025-04-29 16:15:53 +0300

    added testData and RegistrationWithTestDataTests

--- src/test/java/tests/RegistrationWithTestDataTests.java
+++ src/test/java/tests/RegistrationWithTestDataTests.java
@@ -1,24 +1,48 @@
 package tests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static tests.TestData.*;
 
-public class RegistrationTests extends TestBase {
+public class RegistrationWithTestDataTests extends TestBase {
+
+//    String firstName = "Alex",
+//            lastName = "Egorov",
+//            userEmail = "alex@egorov.com";
+
+
+//    static String firstName,
+//            lastName,
+//            userEmail;
+//
+//    @BeforeEach
+//    void prepareTestData() {
+//        firstName = "Alex";
+//        lastName = "Egorov";
+//        userEmail = "alex@egorov.com";
+//    }
 
     @Test
     void successfulRegistrationTest() {
+
+//        String firstName = "Alex",
+//                lastName = "Egorov",
+//                userEmail = "alex@egorov.com";
+
+
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
 
-        $("#firstName").setValue("Alex");
-        $("#lastName").setValue("Egorov");
-        $("#userEmail").setValue("alex@egorov.com");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
         $("#genterWrapper").$(byText("Other")).click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
@@ -37,7 +61,7 @@ public class RegistrationTests extends TestBase {
 
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Alex"), text("Egorov"),
-                text("alex@egorov.com"), text("1234567890"));
+        $(".table-responsive").shouldHave(text(firstName), text(lastName),
+                text(userEmail), text("1234567890"));
     }
 }
---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class TestData {
+
+    public static String firstName = "Alex",
+            lastName = "Egorov",
+            userEmail = "alex@egorov.com";
+
+
+}
