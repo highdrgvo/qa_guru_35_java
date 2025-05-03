31c7dcb09fb3ac2ea687fc3eea2f1b06fb5f5840
sergeyplechko <sergeyplechko@gmail.com>
2025-05-03 12:13:44 +0300

    updated

--- src/test/java/utils/RandomFakerUtils.java
+++ src/test/java/utils/RandomFakerUtils.java
@@ -3,6 +3,7 @@ package utils;
 import com.github.javafaker.Faker;
 
 import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomFakerUtils {
 
@@ -13,6 +14,11 @@ public class RandomFakerUtils {
     public String userEmail = faker.internet().emailAddress();
     public String userGender = faker.;
     public String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
+
+
+    public static int getRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
 
 
 
