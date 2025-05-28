080c80cbc9c4613ccd563205a629f338e517366a
sergeyplechko <sergeyplechko@gmail.com>
2025-05-28 09:37:01 +0300

    updated

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -12,7 +12,6 @@ public class SystemPropertiesTests {
     }
 
     @Test
-    @Tag("jenkinsProperties")
     void systemProperties1Test() {
         System.setProperty("browser", "chrome"); // System - некое внутреннее хранилище ключ значение. Это для того, чтобы забрать значение переменной извне. В тестах это лучше не использовать.
         String browser = System.getProperty("browser");
@@ -21,7 +20,6 @@ public class SystemPropertiesTests {
     }
 
     @Test
-    @Tag("jenkinsProperties")
     void systemProperties2Test() {
         String browser = System.getProperty("browser", "mozilla"); // def - a default value. Если в selenide не передать браузер, он по дефолту возьмет хром. В данном случае у нас по дефолту будет moziila.
 
@@ -29,11 +27,20 @@ public class SystemPropertiesTests {
     }
 
     @Test
-    @Tag("jenkinsProperties")
     void systemProperties3Test() {
         System.setProperty("browser", "chrome");
         String browser = System.getProperty("browser", "mozilla"); // def - a default value. Если в selenide не передать браузер, он по дефолту возьмет хром. В данном случае у нас по дефолту будет moziila.
 
         System.out.println(browser);
+    }
+
+    @Test
+    @Tag("jenkinsProperties")
+    void systemProperties4Test() {
+        String browser = System.getProperty("browser", "mozilla");
+
+        System.out.println(browser);
+
+//        gradle jenkins_properties -Dbrowser=chrome
     }
 }
