a36274790c893d871f10233259f6d6f922bfbc65
sergeyplechko <sergeyplechko@gmail.com>
2025-05-29 21:19:17 +0300

    updated

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -1,8 +1,10 @@
 package properties;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
 import static java.lang.String.format;
 
 public class SystemPropertiesTests {
@@ -55,6 +57,15 @@ public class SystemPropertiesTests {
         System.out.println(message);
 
         // gradle hello_test -Dname="Loh Cvetochnii"
+
+    }
+
+    @Test
+    @Tag("jenkinsParametersHomeWorkTest")
+    void systemPropertiesUrlRemoteBrowserTest() {
+
+        String urlRemoteBrowser = System.getProperty(Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        System.out.println(urlRemoteBrowser);
 
     }
 }
